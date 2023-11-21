8d46cd657e138c4f3cde0743fe1c9ff087dc38c8
ANNIL <oxetaligado@gmail.com>
2023-11-21 14:10:51 -0300

    !a!

---
+++ lista-2/21.py
@@ -0,0 +1,7 @@
+ge = input("digite se seu gênero é f (feminino) ou m (masculino): ")
+if ge == "f":
+    print("seu genero é feminino")
+elif ge == "m":
+    print("seu genero é Masculino")
+else:
+    print("seu genero é indefinido")
